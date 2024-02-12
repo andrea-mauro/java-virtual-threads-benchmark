7128bdafe17d743be180f77d6453846228bf0a98
Andrea Mauro <andrea.mauro.h@gmail.com>
2024-02-12 22:51:36 +0100

    refactor: java and js source code

--- nodejs-demo/src/app.js
+++ nodejs-demo/src/app.js
@@ -1,12 +1,39 @@
 var express = require('express')
 var app = express()
 
-app.get('/demo', function (req, res) {
-    setTimeout(() => {
-        res.send('Hello World!')
-    }, 400)
+app.get('/demo/cpu-light', async function (req, res) {
+
+    const initialTime = new Date().getTime()
+    await someIoBlockingOperation(100)
+    await someIoBlockingOperation(100)
+
+    console.log(`Request processed in ${new Date().getTime() - initialTime} ms`)
+    res.send('Hello World!')
+})
+
+app.get('/demo/cpu-intensive', async function (req, res) {
+
+    const initialTime = new Date().getTime()
+    await someIoBlockingOperation(100)
+    someCpuIntensiveTask(10)
+    await someIoBlockingOperation(100)
+
+    console.log(`Request processed in ${new Date().getTime() - initialTime} ms`)
+    res.send('Hello World!')
 })
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
+
+function someIoBlockingOperation(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+function someCpuIntensiveTask(ms) {
+
+    const now = new Date().getTime()
+    while (now + ms > new Date().getTime()) {
+        // do nothing
+    }
+}
--- spring-boot-demo/src/main/java/com/example/demo/controller/DemoController.java
+++ spring-boot-demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -3,22 +3,52 @@ package com.example.demo.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
+@RequestMapping("/demo")
 public class DemoController {
+
     private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
-    
-    @GetMapping("/demo")
-    public void get() throws InterruptedException {
-        Thread.sleep(200);
-        /** FIXME to simulate some CPU work
-         var currentTime = System.currentTimeMillis();
-         while(currentTime + 10 > System.currentTimeMillis()) {
-         // simulate some intensive CPU work
-         }
-         */
-        Thread.sleep(200);
-        logger.info("Running on Thread {}", Thread.currentThread().getName());
+
+    @GetMapping("/cpu-light")
+    public String getCpuLight() {
+
+        var initialTime = System.currentTimeMillis();
+        someIoBlockingOperation(100);
+        someIoBlockingOperation(100);
+
+        logger.info("Request processed in {} ms", System.currentTimeMillis() - initialTime);
+        return "Hello, World!";
+    }
+
+    @GetMapping("/cpu-intensive")
+    public String getCpuIntensive() {
+
+        var initialTime = System.currentTimeMillis();
+        someIoBlockingOperation(100);
+        someCpuIntensiveTask(10);
+        someIoBlockingOperation(100);
+
+        logger.info("Request processed in {} ms", System.currentTimeMillis() - initialTime);
+        return "Hello, World!";
+    }
+
+    private void someIoBlockingOperation(int ms) {
+
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            logger.error("Error while performing IO operation", e);
+        }
+    }
+
+    private void someCpuIntensiveTask(int ms) {
+
+        var currentTime = System.currentTimeMillis();
+        while (currentTime + ms > System.currentTimeMillis()) {
+            // do nothing
+        }
     }
 }
